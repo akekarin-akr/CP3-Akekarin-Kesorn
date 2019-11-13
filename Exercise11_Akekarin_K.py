c41f1f3dd32d8359cda1e6fcc1ffdc9fa364036e
akekarin-akr <57682032+akekarin-akr@users.noreply.github.com>
2019-11-14 01:22:29 +0700

    Improve the code

--- Exercise11_Akekarin_K.py
+++ Exercise11_Akekarin_K.py
@@ -1,7 +1,5 @@
 inputNumber = int(input("Number : "))
 countSpace = int((inputNumber - 1) / 2)
 for i in range(1, inputNumber+2, 2):
-    space = " " * countSpace
+    print(" " * countSpace, ("*" * i))
     countSpace -= 1
-    print(space, ("*" * i))
-
