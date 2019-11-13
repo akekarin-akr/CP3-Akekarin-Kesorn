83a4a3d09197158548621447720f8aad8ab889da
akekarin-akr <57682032+akekarin-akr@users.noreply.github.com>
2019-11-13 12:01:01 +0700

    Add files via upload

---
+++ Exercise4_Akekarin_K.py
@@ -0,0 +1,10 @@
+score_Foundation_English = 80.4
+score_General_Business = 79.6
+score_Introduction_to_Computer_Systems = 87
+score_Computer_Programming = 88.0
+
+print("---Your Score---")
+print("Foundation English               :", score_Foundation_English)
+print("General Business                 :", score_General_Business)
+print("Introduction to Computer Systems :", score_Introduction_to_Computer_Systems)
+print("Computer Programming             :", score_Computer_Programming)
